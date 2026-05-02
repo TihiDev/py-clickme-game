3490b9878449dcc8884657347b0ba3f0eb20c363
TihiDev <tihamerdragomer0@gmail.com>
2026-05-02 11:54:08 +0300

    Added reset score button

--- main.py
+++ main.py
@@ -11,10 +11,17 @@ def increase_score():
     score += 1 
     label.config( text=f"Pontszam: {score}" )
 
+def reset_score(): 
+    global score 
+    score = 0
+    label.config( text=f"Pontszam: {score}" )
+
 label = tk.Label( root, text="Pontszam: 0", font=("Arial", 20) )
 label.pack( pady=20 )
 
 button = tk.Button( root, text="Kattints ram!", font=("Arial", 14), command=increase_score )
-button.pack()
+buttonrst = tk.Button( root, text="Pontszam nullazas", font=("Arial", 14), command=reset_score )
 
+button.pack()
+buttonrst.pack()
 root.mainloop()
